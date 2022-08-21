c4f01bbe96a3afab80c1c87a1d32ffb42f71b557
CryoVit <cryovit@outlook.com>
2022-08-21 20:07:05 +0800

    Update 220821

--- intergrator.py
+++ intergrator.py
@@ -20,11 +20,11 @@ def parsefile (file_path):
     data = pd.DataFrame(ldict, columns =['uid', 'room_id', 'name', 'guard_num'])
     fulldata = pd.concat([fulldata, data])
 
-date = '220813/'
+date = '220821/'
 
 getfile(241, 10, date + '0.json')
-getfile(75, 60, date + '1.json')
-getfile(76, 850, date + '2.json')
+getfile(75, 100, date + '1.json')
+getfile(76, 900, date + '2.json')
 
 parsefile(date + '0.json')
 parsefile(date + '1.json')
